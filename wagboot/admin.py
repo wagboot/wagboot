d8421c5839d46b6fadbca59a8673dfe92106279c
Dmitry Ulupov <dima@ulupov.com>
2016-02-18 10:05:28 +0800

    fixed admin for Css models

--- wagboot/admin.py
+++ wagboot/admin.py
@@ -1,13 +1,18 @@
 # Register your models here.
-from django.contrib.admin import ModelAdmin
+from django.contrib import admin
+
+from wagboot.models import Css
 
 try:
     from reversion.admin import VersionAdmin
     base_admin = VersionAdmin
 except ImportError:
     # django_reversion is not available
-    base_admin = ModelAdmin
+    base_admin = admin.ModelAdmin
 
 
 class CssAdmin(base_admin):
     pass
+
+
+admin.site.register(Css, CssAdmin)
