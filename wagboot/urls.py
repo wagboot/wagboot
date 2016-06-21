63f8754e1721aa57e09a82b3180f90402013d021
Dmitry Ulupov <dima@ulupov.com>
2016-06-21 12:53:21 +0800

    added redirect_to_login helper view

--- wagboot/urls.py
+++ wagboot/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^robots.txt', 'wagboot.views.robots_txt', name='robots_txt'),
+    url(r'^redirect-to-login', 'wagboot.views.redirect_to_login', name='redirect_to_login'),
 ]
--- wagboot/views.py
+++ wagboot/views.py
@@ -10,3 +10,15 @@ def robots_txt(request):
     website_settings = WebsiteSettings.for_site(request.site)
 
     return HttpResponse(content=website_settings.robots_txt or "Allow /\n", content_type="text/plain")
+
+
+def redirect_to_login(request):
+    """
+    We don't have fixed login view.
+    So this view redirects to the currently selected login page in WebsiteSettings.login_page.
+    If it does not exists it redirects to root.
+    """
+
+    login_url = WebsiteSettings.get_login_url(request.site)
+    return login_url or '/'
+
