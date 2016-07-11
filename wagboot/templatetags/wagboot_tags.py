6cb958fecd09c02f6ad9bc6cd88b3206469bd5a9
Dmitry Ulupov <dima@ulupov.com>
2016-07-11 14:04:31 +0800

    added helper tag for image with variable, ref https://github.com/torchbox/wagtail/issues/2090

--- wagboot/templatetags/wagboot_tags.py
+++ wagboot/templatetags/wagboot_tags.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django import template
+from wagtail.wagtailimages.templatetags.wagtailimages_tags import ImageNode
 
 register = template.Library()
 
@@ -13,3 +14,24 @@ def add_class_to_field(bound_field, klass):
         klass = "{} {}".format(existing, klass)
 
     return bound_field.as_widget(attrs={"class": klass})
+
+
+# This works the same as wagtail built-in, but accepts variable in filter spec
+# Should not be needed after this is resolved https://github.com/torchbox/wagtail/issues/2090
+@register.tag(name="image_with_variables")
+def image(parser, token):
+    from wagtail.wagtailimages.templatetags.wagtailimages_tags import image
+    image_node = image(parser=parser, token=token)
+    return ImageNodeWithVariables(image_expr=image_node.image_expr,
+                                  output_var_name=image_node.output_var_name,
+                                  attrs=image_node.attrs,
+                                  filter_spec=image_node.filter_spec)
+
+
+class ImageNodeWithVariables(ImageNode):
+    def render(self, context):
+        try:
+            self.filter_spec = template.Variable(self.filter_spec).resolve(context)
+        except template.VariableDoesNotExist:
+            pass
+        return super(ImageNodeWithVariables, self).render(context)
