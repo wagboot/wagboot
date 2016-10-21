37eb3dfe6c97a5d49da2dfb3ea946b5b2f15de81
Dmitry Ulupov <dima@ulupov.com>
2016-10-21 09:36:24 +0800

    added support of django 1.10 in middleware

--- wagboot/middleware.py
+++ wagboot/middleware.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+import django
 from django.http import HttpResponsePermanentRedirect
 from django.http import HttpResponseRedirect
 
 from wagboot.exceptions import RedirectException
 
+if django.VERSION >= (1, 10):
+    from django.utils.deprecation import MiddlewareMixin
+else:
+    MiddlewareMixin = object
 
-class RedirectMiddleware(object):
+
+class RedirectMiddleware(MiddlewareMixin):
 
     @staticmethod
     def process_exception(request, exception):
